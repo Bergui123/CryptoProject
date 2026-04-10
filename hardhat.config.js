631f27d2b21a300dcdcfa6807415c5394ccc97c8
ethanmckean <113357040+ethanmckean@users.noreply.github.com>
2026-04-10 18:48:25 +0800

    fix: register Hardhat 3 plugins correctly so Mocha tests run

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,23 @@
-import "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-export default {
-  solidity: "0.8.28",
-  networks: {
-    hardhat: {
-      type: "edr-simulated"
-    },
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
-    }
+const networks = {
+  hardhat: {
+    type: "edr-simulated"
   }
+};
+
+if (process.env.SEPOLIA_RPC_URL) {
+  networks.sepolia = {
+    type: "http",
+    url: process.env.SEPOLIA_RPC_URL,
+    accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+  };
+}
+
+export default {
+  plugins: [hardhatToolboxMochaEthers],
+  solidity: "0.8.28",
+  networks
 };
